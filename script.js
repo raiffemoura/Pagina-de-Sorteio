2cb194c8d044f99de3e905db1dbd1f671219e39b
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2023-08-09 10:40:22 -0300

    Página de Sorteio

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function numb(){
+    number = document.querySelector("#res").value;
+    
+}
+
+function resul() {
+
+    let s = document.getElementsByTagName("h2")[0].innerHTML = "O número sorteado foi..." + Math.round(Math.random()*number)
+   
+}
+
+
+
