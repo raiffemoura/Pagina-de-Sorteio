2c7b10267d810bd748284fa004b4770fcdbd1862
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2023-08-31 01:25:30 -0300

    pessoas

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ function numb(){
 
 function resul() {
 
-    let s = document.getElementsByTagName("h2")[0].innerHTML = "O número sorteado foi..." + Math.round(Math.random()*number)
+    let s = document.getElementsByTagName("h2")[0].innerHTML = "O número sorteado foi..." + Math.floor(Math.random()*number+1)
    
 }
 
+function resNomes() {
+    let lista = []
+    let nomes = document.querySelector("#lista").value
+    lista.push(nomes)
+    
 
+
+
+
+    console.log(lista[0])
+}
 
