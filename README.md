fc137ea4abc0d6781744f0e7f90d15cb10e4f18a
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2023-09-04 17:09:01 -0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Pagina-de-Sorteio
 Pagina de sorteio de numero e nomes.
 
-Nesse desafio, tive que criar uma página que fosse capaz de sortear um numero ou nome aleatorio.
+Nesse desafio, tive que criar uma página que fosse capaz de sortear um numero ou nome aleatorio de uma lista fornecida pelo usuario.
--- script.js
+++ script.js
@@ -1,24 +1,36 @@
+var modal = document.querySelector("dialog")
+var buttonClose = document.querySelector("dialog button")
 
-function numb(){
+function numb() {
     number = document.querySelector("#res").value;
-    
+
 }
 
 function resul() {
 
-    let s = document.getElementsByTagName("h2")[0].innerHTML = "O número sorteado foi..." + Math.floor(Math.random()*number+1)
-   
-}
+    let s = document.getElementsByTagName("h2")[0].innerHTML = "O número sorteado foi..." + Math.floor(Math.random() * number + 1)
 
+}
+var listaNomes = []
 function resNomes() {
-    let lista = []
+
     let nomes = document.querySelector("#lista").value
-    lista.push(nomes)
-    
+    nomes.split(",").forEach(element => {
+        console.log(element)
+        listaNomes.push(element)
+    });
+    console.log(listaNomes)
+    var sorteio = listaNomes.length
+    console.log("Tamanho do array é " + sorteio)
+    var sorteado = listaNomes[Math.floor(Math.random()*sorteio)]
+
+   
+    alert("O nome sorteado foi... " + sorteado)
+
+    listaNomes = []
+};
 
 
 
 
-    console.log(lista[0])
-}
 
